ce62ef5e088f4295ce275c6e33c0340ce09a9c54
Rodrigo José <rdrgcornejo@hotmail.com>
2023-08-18 10:57:14 -0300

    listagem vendedores

--- src/store/usuarios.ts
+++ src/store/usuarios.ts
@@ -85,6 +85,35 @@ export const usuarioRecords = {
           });
       });
   },
+  listaVendedores: (pagination: string, dados: any) => {
+    const url = localStorage.getItem("urlAPI");
+    return fetch(url + "/users/list?"+pagination, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": stateGlobal.token,
+      },
+      body: JSON.stringify(dados),
+    }).then(function (response) {
+        if (response.status == 200) {
+          return response.json();
+        }else if (response.status == 401) {
+          notification.notify({
+            title: "Não autenticado",
+            text: "Faça o login novamente",
+            type: "error",
+          });
+          window.location.href = window.location.href.split("#")[0] + "#/login";
+        }
+      }).catch(function (error) {
+        console.log("response error ", error)
+        notification.notify({
+            title: "ERRO",
+            text: error,
+            type: "error",
+          });
+      });
+  },
 
 
     
--- src/views/vendedor.vue
+++ src/views/vendedor.vue
@@ -3,13 +3,68 @@
     <div class="m-5">
       <input class="border rounded py-2 px-3 w-full text-black bg-white border-black" type="text" placeholder="Pesquisar"
         v-model="pesquisa" />
-
     </div>
+
+    <div class="m-5 bg-white border-black h-fit border rounded cursor-pointer ">
+      <div v-for="vendedor in vendedores" :key="vendedor.id" class=" relative " @click="detalha(vendedor.id)">
+        <div class="pl-2 py-1 ">
+          <p class="text-lg">{{ vendedor.nome }} {{ vendedor.sobrenome }}</p>
+          <p class="text-xs">
+            {{ vendedor.email }}
+          </p>
+        </div>
+        <ul class="space-y-2 border-t border-gray-200 dark:border-gray-700"></ul>
+      </div>
+    </div>
+
+    <paginacao class="m-5" :itens-pages="10" :total-pages="totalPaginas" @update="buscaVendedores" ref="pagination">
+    </paginacao>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch, onMounted, Ref } from "vue";
+import { usuarioRecords } from "../store/usuarios";
+import paginacao from "../components/paginacao.vue";
 
+interface vendBody {
+  id: number
+  nome: string
+  sobrenome: string
+  email: string
+  ativo: boolean
+}
+
+const pagination = ref(null)
 let pesquisa = ref("");
+let totalPaginas = ref(0)
+let vendedores = ref([]) as Ref<vendBody[]>;
+
+onMounted(() => {
+  buscaVendedores("page=0&size=10");
+})
+
+watch(pesquisa, async () => {
+  buscaVendedores('page=0&size=10')
+  // @ts-ignore: Object is possibly 'null'.
+  pagination.value.reinicia()
+})
+
+const buscaVendedores = async (paginacao: string) => {
+  let body = {
+    pesquisa: pesquisa.value,
+    status: ""
+  }
+  console.log("pesquisa ", pesquisa.value)
+
+  let response = await usuarioRecords.listaVendedores(paginacao, body);
+  if (response.sucess == true) {
+    vendedores.value = response.body.content;
+  }
+  totalPaginas.value = response.body.totalPages
+}
+
+const detalha = async (codigo: number) => {
+  console.log("detalaha ", codigo)
+}
 </script>
